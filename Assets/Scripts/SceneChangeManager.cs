6d50d9790a14e7b6f7f2e0bd2abe5ae0bbde157d
suzy <si.jogg72@skkukdp.re.kr>
2024-04-17 13:08:25 +0900

    scene 전환 스크립트 수정 및 씬 전환 추가

--- Assets/Scripts/SceneChangeManager.cs
+++ Assets/Scripts/SceneChangeManager.cs
@@ -8,6 +8,11 @@ public class SceneChangeManager : MonoBehaviour
 
     // 화면 씬 전환 스크립트
 
+    //홈 씬으로 전환
+    public void HomeSceneChange()
+    {
+        SceneManager.LoadScene("basicScene_Sensor");
+    }
 
     //등록 씬으로 전환
     public void BasicSceneChange()
@@ -21,16 +26,21 @@ public class SceneChangeManager : MonoBehaviour
         SceneManager.LoadScene("ReportChatScene");
     }
 
-
-    public void SceneChange2()
-    {
-        SceneManager.LoadScene("basicScene_Sensor");
-    }
-
-    public void SceneChange3()
+    //카메라 씬으로 전환
+    public void CameraSceneChange()
     {
         SceneManager.LoadScene("CameraScene");
     }
+
+    //어플리케이션 종료
+    public void SceneQuit()
+    {
+        Application.Quit();
+
+    }
+
+
+
 
 
 }
