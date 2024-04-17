6d63ad09699586481b250f58cfeeb0a16299c4bb
suzy <si.jogg72@skkukdp.re.kr>
2024-04-17 12:48:10 +0900

    씬 전환 구현
    - 스크립트 함수 추가 및 빌드에서 버튼 메서드 변경

--- Assets/Scripts/SceneChangeManager.cs
+++ Assets/Scripts/SceneChangeManager.cs
@@ -7,6 +7,21 @@ public class SceneChangeManager : MonoBehaviour
 {
 
     // 화면 씬 전환 스크립트
+
+
+    //등록 씬으로 전환
+    public void BasicSceneChange()
+    {
+        SceneManager.LoadScene("MapBasicScene");
+    }
+
+    //채팅 씬으로 전환
+    public void ChatSceneChange()
+    {
+        SceneManager.LoadScene("ReportChatScene");
+    }
+
+
     public void SceneChange2()
     {
         SceneManager.LoadScene("basicScene_Sensor");
